1f0d35479a2fd92686283ddd5a1ecb732ee47d23
sheetalc818 <sheetalc818@gmail.com>
2023-02-08 22:30:47 +0530

    Added Script: Phone no Validation

---
+++ src/main/java/com/bridgelabz/ejet/base/BaseClass.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.ejet.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+    public WebDriver driver;
+
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://bldev.bridgelabz.com/webinar");
+    }
+}
