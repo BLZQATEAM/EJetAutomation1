a5113a9cf1999ffe6ea01ec49d4431d8ca615576
sheetalc818 <sheetalc818@gmail.com>
2023-02-13 11:14:39 +0530

    Added : Propertise file Read script

--- src/main/java/com/bridgelabz/ejet/base/BaseClass.java
+++ src/main/java/com/bridgelabz/ejet/base/BaseClass.java
@@ -4,14 +4,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BaseClass {
+public class BaseClass extends ReadPropertiesFile {
     public WebDriver driver;
 
     public void setUp() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://bldev.bridgelabz.com/attendorientation?fc=Scholarship&sc=WBR-SCH-EMAIL&ab_webinar=true&id=");
+        //driver.get("https://bldev.bridgelabz.com/attendorientation?fc=Scholarship&sc=WBR-SCH-EMAIL&ab_webinar=true&id=");
+        driver.get(properties.getProperty("webinarLandingPage_URL"));
         Thread.sleep(1000);
     }
 }
--- src/main/java/com/bridgelabz/ejet/base/ReadPropertiesFile.java
+++ src/main/java/com/bridgelabz/ejet/base/ReadPropertiesFile.java
@@ -1,2 +1,27 @@
-package com.bridgelabz.ejet.base;public class ReadPropertiesFile {
+package com.bridgelabz.ejet.base;
+
+
+import java.io.*;
+import java.util.Properties;
+
+public class ReadPropertiesFile {
+    public Properties properties;
+    private final String propertyFilePath= "C:\\Users\\dell\\Documents\\QADevPrograms\\EJetTestScript\\src\\main\\java\\com\\bridgelabz\\ejet\\testdata\\TestData.propertise";
+
+    public ReadPropertiesFile(){
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(propertyFilePath));
+            properties = new Properties();
+            try {
+                properties.load(reader);
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
+        }
+    }
 }
