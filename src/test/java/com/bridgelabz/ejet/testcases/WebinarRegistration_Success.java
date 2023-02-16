b5078634062de67a6a2a45dc624783ba19dd9104
sheetalc818 <sheetalc818@gmail.com>
2023-02-16 18:56:25 +0530

    Added : Script to check Existing Phone Number and New User

---
+++ src/test/java/com/bridgelabz/ejet/testcases/WebinarRegistration_Success.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.ejet.testcases;
+
+import com.bridgelabz.ejet.base.BaseClass;
+import com.bridgelabz.ejet.pages.WebinarRegistration_Page;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.io.IOException;
+
+public class WebinarRegistration_Success extends BaseClass {
+    WebinarRegistration_Page page;
+    @BeforeMethod
+    public void setUp() throws InterruptedException, IOException {
+        initialization();
+        page = new WebinarRegistration_Page(driver);
+    }
+
+    @Test
+    public void webinarUserRegistrationSuccess_AndRedirectOnAttendOrientationScreen() throws InterruptedException {
+        page.enterData_InFields();
+        boolean msg = page.clickEnter();
+        Thread.sleep(3000);
+        Assert.assertTrue(msg);
+    }
+
+    @AfterMethod
+    public void close_Browser(){
+        driver.close();
+    }
+}
