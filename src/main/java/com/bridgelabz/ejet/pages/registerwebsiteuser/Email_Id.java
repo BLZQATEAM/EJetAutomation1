b6e54883d6189317eb1708087dfaebe9aacb25fc
sheetalc818 <sheetalc818@gmail.com>
2023-03-26 23:06:50 +0530

    Modified : Script for RegisterWebsiteUser Screen

---
+++ src/main/java/com/bridgelabz/ejet/pages/registerwebsiteuser/Email_Id.java
@@ -0,0 +1,54 @@
+package com.bridgelabz.ejet.pages.registerwebsiteuser;
+
+import com.bridgelabz.ejet.base.ReadWebinarPropertiesFile;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.io.IOException;
+
+public class Email_Id extends ReadWebinarPropertiesFile {
+    @FindBy(id="deftemail")
+    WebElement email_id;
+    @FindBy(id="deftemailvalid")
+    WebElement invalid_email;
+    @FindBy(xpath="//p[text()='Email Required']")
+    WebElement email_req;
+    public Email_Id(WebDriver driver) throws IOException {
+        this.driver=driver;
+        PageFactory.initElements(driver, this);
+    }
+    public boolean valid_Email_Id(String valid_emailid) throws InterruptedException {
+        email_id.sendKeys(valid_emailid);
+        Thread.sleep(3000);
+        String error_msg = invalid_email.getText();
+        boolean value=false;
+        if (error_msg.equals("Invalid Email")) {
+            value= invalid_email.isDisplayed();
+
+        } else if (error_msg.equals("Email Required")) {
+            value= email_req.isDisplayed();
+        }
+        return value;
+    }
+    public boolean invalid_Email_Id(String valid_emailid) throws InterruptedException {
+        email_id.sendKeys(valid_emailid);
+        Thread.sleep(3000);
+        String error_msg=invalid_email.getText();
+        boolean value=invalid_email.isDisplayed();
+        return value;
+    }
+    public boolean enter_And_Remove_EmailId(String valid_emailid) throws InterruptedException {
+        email_id.sendKeys(valid_emailid);
+        Thread.sleep(3000);
+        for (int i=0;i<valid_emailid.length();i++){
+            email_id.sendKeys(Keys.BACK_SPACE);
+        }
+        Thread.sleep(3000);
+        String error_msg=invalid_email.getText();
+        boolean value=email_req.isDisplayed();
+        return value;
+    }
+}
